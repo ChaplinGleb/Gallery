b084258435e66d0570b629c343ce684c43012c2e
Gleb <68479882+ChaplinGleb@users.noreply.github.com>
2023-12-03 10:39:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let result = [];
 let low = [];
-let num = 100;
+let num = 20;
 
 document.querySelector('button[type="submit"]').addEventListener('click', () => {
     let links = document.querySelector('textarea').value.split(/\r?\n/);
