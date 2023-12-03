e85774994f8c1c6e1f7dbb10209279a6f4190d9b
Gleb <68479882+ChaplinGleb@users.noreply.github.com>
2023-12-03 10:46:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,59 @@
-let result = [];
-let low = [];
-let num = 100;
-
+let result = []
+let low = []
+let num = 100
 document.querySelector('button[type="submit"]').addEventListener('click', () => {
-    let links = document.querySelector('input[type="textarea"]').value.split(/\r?\n/);
+    let links = document.querySelector('input[type="textarea"]').value.split('http')
 
     if (links.length > num) {
         for (let i = 0; i < links.length; i++) {
-            if (links[i] !== '') {
+            if (links[i] != '') {
                 if (i < num) {
-                    low.push('http' + links[i]);
+                    low.push('http' + links[i])
                 } else {
-                    result.push(low);
-                    low = [];
-                    num += 100;
+                    result.push(low)
+                    low = []
+                    num += 100
                 }
             }
         }
 
-        createPage(0);
+        createPage(0)
 
-        document.querySelector('.pagination').innerHTML = '';
+        document.querySelector('.pagination').innerHTML = ''
         for (let i = 0; i < result.length; i++) {
             document.querySelector('.pagination').innerHTML += `
-            <div class="pagination__page" id="page-${i}">${i + 1}</div>`;
+            <div class="pagination__page" id="page-${i}">${i + 1}</div>`
         }
-        document.querySelector('#page-0').classList.add('active');
+        document.querySelector('#page-0').classList.add('active')
 
         document.querySelectorAll('.pagination__page').forEach(el => {
             el.addEventListener('click', () => {
-                createPage(el.id.slice(-1));
-                document.querySelector('.active').classList.remove('active');
-                el.classList.add('active');
-            });
-        });
+                createPage(el.id.slice(-1))
+                document.querySelector('.active').classList.remove('active')
+                el.classList.add('active')
+            })
+        })
     } else {
         for (let i = 0; i < links.length; i++) {
-            if (links[i] !== '') {
-                result.push('http' + links[i]);
+            if (links[i] != '') {
+                result.push('http' + links[i])
             }
         }
-        createPage(0);
+        createPage(0)
     }
-});
+})
 
 function createPage(number) {
-    document.querySelector('.gallery').innerHTML = '';
+    document.querySelector('.gallery').innerHTML = ''
 
-    if (typeof result[number] === 'object') {
+    if (typeof result[number] == Object) {
         for (let i = 0; i < result[number].length; i++) {
             document.querySelector('.gallery').innerHTML += `
                 <div class="gallery__item">
                     <div class="gallery__img">
                         <img src="${result[number][i]}" alt="">
                     </div>
-                </div>`;
+                </div>`
         }
     } else {
         for (let i = 0; i < result.length; i++) {
@@ -63,13 +62,13 @@ function createPage(number) {
                     <div class="gallery__img">
                         <img src="${result[i]}" alt="">
                     </div>
-                </div>`;
+                </div>`
         }
     }
 
     document.querySelectorAll('.gallery__item').forEach(el => {
         el.addEventListener('click', () => {
-            window.open(el.querySelector('img').getAttribute('src'));
-        });
-    });
+            window.open(el.querySelector('img').getAttribute('src'))
+        })
+    })
 }
